936bf831356bcc025f085372f206b4d083336c67
Fani Annur Rozi <crazyhx4@gmail.com>
2025-12-09 11:01:59 +0700

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,13 +4,11 @@ module.exports = {
     script: './backend/server.js',
     instances: 1,
     autorestart: true,
+    restart_delay: 3000,
+    max_restart: 10,
     watch: false,
     max_memory_restart: '1G',
     env: {
-      NODE_ENV: 'development',
-      PORT: 3001
-    },
-    env_production: {
       NODE_ENV: 'production',
       PORT: 3001
     },
